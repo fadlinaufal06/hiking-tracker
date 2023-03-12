9092e84e97ebe0c7f0d705c655afe010559d7775
fadlinaufaal <fadlinaufalr06@gmail.com>
2023-03-12 20:34:02 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState} from 'react';
+
+
+
+
+
+function App() {
+  const[count, setCount] = useState(4)
+
+  function decrementCount(){
+    setCount(prevCount => prevCount-1)
+  }
+  function incrementCount(){
+    setCount(prevCount => prevCount+1)
+  }
+
+  return (
+    <div className="App">
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+    </div>
+  );
+}
+
+export default App;

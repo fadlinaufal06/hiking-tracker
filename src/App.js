50392570acd3718c2124e0142deb4abbed222756
fadlinaufaal <fadlinaufalr06@gmail.com>
2023-05-27 19:39:51 +0700

    fix bug on search

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ function App() {
           <Header/>
         </div>
 
-      <div className="relative w-full">
+        <div className="relative w-full">
         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
           <FaSearch className="h-5 w-5 text-gray-400" />
         </div>
@@ -88,10 +88,16 @@ function App() {
         />
       </div>
 
-        {Object.entries(filteredData).map(([id, health]) => (
+      {searchQuery.trim() === "" ? (
+        Object.entries(data).map(([id, health]) => (
           <Card key={id} id={id} health={health} />
-        ))}
-      </div>
+        ))
+      ) : (
+        Object.entries(filteredData).map(([id, health]) => (
+          <Card key={id} id={id} health={health} />
+        ))
+      )}
+    </div>
 
     <MapContainer center={currentPosition} zoom={20} className="flex-1">
       <Marker position={currentPosition}>
